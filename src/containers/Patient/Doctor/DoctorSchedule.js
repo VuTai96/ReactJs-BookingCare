10af29630a1f45b7e671234638c89995a78cd940
Vu Van Tai <vutai13196@gmail.com>
2022-08-10 16:34:11 +0700

    77 layout component DoctorSchedule 1

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -8,12 +8,14 @@ import { getScheduleDoctorByDate } from '../../../services/userService'
 import { withRouter } from 'react-router-dom'
 
 
+
 class DoctorSchedule extends Component {
     constructor(props) {
         super(props)
         this.state = {
             allDays: [],
-            dateSelected: moment().startOf('day').valueOf()
+            dateSelected: moment().startOf('day').valueOf(),
+            arrScheduleOfDay: []
         }
     }
     setAllDay = (lang) => {
@@ -22,14 +24,32 @@ class DoctorSchedule extends Component {
                 "Chủ nhật", "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7"
             ]
         });
+        moment.updateLocale('homnay', {
+            weekdays: [
+                "Hôm nay", "Hôm nay", "Hôm nay", "Hôm nay", "Hôm nay", "Hôm nay", "Hôm nay"
+            ]
+        });
+        moment.updateLocale('today', {
+            weekdays: [
+                "today", "today", "today", "today", "today", "today", "today"
+            ]
+        });
         let arrDate = []
         for (let i = 0; i < 7; i++) {
             let obj = {}
             if (lang === LANGUAGES.VI) {
-                obj.label = moment().add(i, 'days').locale('vi').format('dddd - DD/MM')
+                if (i === 0) {
+                    obj.label = moment().add(i, 'days').locale('homnay').format('dddd - DD/MM')
+                } else {
+                    obj.label = moment().add(i, 'days').locale('vi').format('dddd - DD/MM')
+                }
             }
             if (lang === LANGUAGES.EN) {
-                obj.label = moment().add(i, 'days').locale('en').format('dddd - DD/MM')
+                if (i === 0) {
+                    obj.label = moment().add(i, 'days').locale('today').format('dddd - DD/MM')
+                } else {
+                    obj.label = moment().add(i, 'days').locale('en').format('dddd - DD/MM')
+                }
             }
             obj.value = moment().add(i, 'days').startOf('day').valueOf()
             arrDate.push(obj)
@@ -41,7 +61,11 @@ class DoctorSchedule extends Component {
             allDays: this.setAllDay(this.props.language)
         })
         let response = await getScheduleDoctorByDate(this.props.match.params.id, this.state.dateSelected)
-        console.log('response', response)
+        if (response.errCode === 0 && response?.data?.length > 0) {
+            this.setState({
+                arrScheduleOfDay: response.data || []
+            })
+        }
 
     }
     componentDidUpdate(prevProps, revState, snapshot) {
@@ -57,17 +81,25 @@ class DoctorSchedule extends Component {
             dateSelected: e.target.value
         })
         let response = await getScheduleDoctorByDate(this.props.match.params.id, e.target.value)
-        console.log('response1', response)
+        console.log('response', response)
+        if (response.errCode === 0) {
+            console.log('response', response)
+
+            this.setState({
+                arrScheduleOfDay: response?.data || []
+            })
+        }
     }
 
     render() {
-        let { allDays, dateSelected } = this.state
+        let { allDays, dateSelected, arrScheduleOfDay } = this.state
+        let { language } = this.props
+        console.log(arrScheduleOfDay)
         return (
             <div className='doctor-schedule-container'>
                 <div className='select-date'>
                     <select className="form-select" aria-label="Default select example"
                         onChange={(e) => this.handleOnchange(e)}
-                    // value={dateSelected}
                     >
                         {allDays.map((item, index) => {
                             return (
@@ -78,7 +110,24 @@ class DoctorSchedule extends Component {
                         })}
                     </select>
                 </div>
-                <div className='show-choosed-date'></div>
+                <div className='schedule-title'>
+                    <i class="fas fa-calendar-alt"> <span>lịch khám</span></i>
+                </div>
+                <div className='show-choosed-date'>
+                    {arrScheduleOfDay.length > 0 ?
+                        arrScheduleOfDay.map((item, index) => {
+                            return (
+                                <button className='btn btn-warning timeType-btn' key={index}>
+                                    {language === LANGUAGES.VI && item.timeTypeData.valueVi}
+                                    {language === LANGUAGES.EN && item.timeTypeData.valueEn}
+
+                                </button>
+                            )
+                        })
+                        :
+                        <div className='timeType-none'>Chưa có lịch khám, vui lòng chọn thời gian khác</div>
+                    }
+                </div>
             </div>
         );
     }
